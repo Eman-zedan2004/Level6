8de1bac2a1606cf98fbe9389ffb657b8b2bff30c
Eman Zedan <emanzedan568@gmail.com>
2026-07-16 12:23:11 +0300

    Display data

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,57 +5,64 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import { useGetProductByNameQuery } from "../redux/ProductAPI";
-
-const receviedDateFromAPI = [{}, {}, {}, {}];
+import "./Home.css";
 
 export default function Home() {
   const { data, error, isLoading } = useGetProductByNameQuery("bulbasaur");
-  console.log(data);
   const theme = useTheme();
 
-  return (
-    <Stack
-      direction={"row"}
-      sx={{ flexWrap: "wrap", justifyContent: "center" }}
-    >
-      {receviedDateFromAPI.map((item) => {
-        return (
-          <Card sx={{ maxWidth: 277, mb: 6, mx: 2 }}>
-            <CardMedia
-              component="img"
-              height="194"
-              image="https://images.pexels.com/photos/18488300/pexels-photo-18488300.jpeg?auto=compress&cs=tinysrgb&w=800"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests.
-              </Typography>
-            </CardContent>
-            <CardActions
-              sx={{ justifyContent: "space-between" }}
-              disableSpacing
+  if (data) {
+    return (
+      <Stack
+        direction={"row"}
+        sx={{ flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {data.map((item) => {
+          return (
+            <Card
+              className="card"
+              key={item.id}
+              sx={{
+                maxWidth: 277,
+                mb: 6,
+                mx: 2,
+              }}
             >
-              <Button
-                sx={{ textTransform: "capitalize", p: 1, lineHeight: 1.1 }}
-                variant="contained"
-                color="primary"
+              <CardMedia
+                component="img"
+                height="277"
+                image={item.imageLink}
+                alt="Paella dish"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{ justifyContent: "space-between" }}
+                disableSpacing
               >
-                Add to cart
-              </Button>
+                <Button
+                  sx={{ textTransform: "capitalize", p: 1, lineHeight: 1.1 }}
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to cart
+                </Button>
 
-              <Typography
-                sx={{ mr: 1 }}
-                variant="body1"
-                color={theme.palette.error.light}
-              >
-                $100
-              </Typography>
-            </CardActions>
-          </Card>
-        );
-      })}
-    </Stack>
-  );
+                <Typography
+                  sx={{ mr: 1 }}
+                  variant="body1"
+                  color={theme.palette.error.light}
+                >
+                  ${item.price}
+                </Typography>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Stack>
+    );
+  }
 }
