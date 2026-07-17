666b7b6b868cf0e180fe771886a19d88e9a5a500
Eman Zedan <emanzedan568@gmail.com>
2026-07-17 13:10:54 +0300

    Update Quantity in LocalStorge

--- Frontend/src/redux/CartSlice.js
+++ Frontend/src/redux/CartSlice.js
@@ -2,8 +2,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  selectedProducts: localStorage.getItem("selectProduct") ? JSON.parse(localStorage.getItem("selectProduct")) : [],
-  selectedProductsID: localStorage.getItem("selectProductID") ? JSON.parse(localStorage.getItem("selectProductID")) : [],
+  selectedProducts: localStorage.getItem("selectProduct")
+    ? JSON.parse(localStorage.getItem("selectProduct"))
+    : [],
+  selectedProductsID: localStorage.getItem("selectProductID")
+    ? JSON.parse(localStorage.getItem("selectProductID"))
+    : [],
 };
 
 export const CartSlice = createSlice({
@@ -14,46 +18,84 @@ export const CartSlice = createSlice({
       const productAndQuantity = { ...action.payload, quantity: 1 };
       state.selectedProducts.push(productAndQuantity);
       state.selectedProductsID.push(action.payload.id);
+
       localStorage.setItem(
         "selectProduct",
         JSON.stringify(state.selectedProducts),
       );
+
       localStorage.setItem(
         "selectProductID",
         JSON.stringify(state.selectedProductsID),
       );
     },
+
     increaseQuantity: (state, action) => {
       const increaseProduct = state.selectedProducts.find((item) => {
         return item.id === action.payload.id;
       });
       increaseProduct.quantity += 1;
+
+      localStorage.setItem(
+        "selectProduct",
+        JSON.stringify(state.selectedProducts),
+      );
+
+      localStorage.setItem(
+        "selectProductID",
+        JSON.stringify(state.selectedProductsID),
+      );
     },
+
     decreaseQuantity: (state, action) => {
       const decreaseProduct = state.selectedProducts.find((item) => {
         return item.id === action.payload.id;
       });
       decreaseProduct.quantity -= 1;
+
       if (decreaseProduct.quantity === 0) {
         const newArr = state.selectedProducts.filter((item) => {
           return item.id !== action.payload.id;
         });
         state.selectedProducts = newArr;
+
         const newArr2 = state.selectedProductsID.filter((item) => {
           return item !== action.payload.id;
         });
         state.selectedProductsID = newArr2;
       }
+
+      localStorage.setItem(
+        "selectProduct",
+        JSON.stringify(state.selectedProducts),
+      );
+
+      localStorage.setItem(
+        "selectProductID",
+        JSON.stringify(state.selectedProductsID),
+      );
     },
+
     deleteProduct: (state, action) => {
       const newArr = state.selectedProducts.filter((item) => {
         return item.id !== action.payload.id;
       });
       state.selectedProducts = newArr;
-      const newArr2 = state.selectedProductsID.filter((id) => {
-        return id !== action.payload.id;
+
+      const newArr2 = state.selectedProductsID.filter((item) => {
+        return item !== action.payload.id;
       });
       state.selectedProductsID = newArr2;
+
+      localStorage.setItem(
+        "selectProduct",
+        JSON.stringify(state.selectedProducts),
+      );
+
+      localStorage.setItem(
+        "selectProductID",
+        JSON.stringify(state.selectedProductsID),
+      );
     },
   },
 });
